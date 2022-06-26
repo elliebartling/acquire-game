534bae870611e7a21ae5d243b589e00a8e18095a
Ellen Marie Bartling <ellenbartling@gmail.com>
2022-06-26 09:12:34 -0700

    Update Nav.vue

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -62,7 +62,7 @@
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                 </svg>
               </button>
-              <router-link v-if="user && user.hasActiveGame" to="/current-game" class="button button-sm hover:text-white primary mr-2 font-medium ml-4">Re-Join Current Game</router-link>
+              <!-- <router-link v-if="user" to="/current-game" class="button button-sm hover:text-white primary mr-2 font-medium ml-4">Re-Join Current Game</router-link> -->
               <button class="button button-sm block secondary" @click="authStore.signOut()" :disabled="loading">
                 Sign Out
               </button>
