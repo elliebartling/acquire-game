ebf72c4d518da2d55057bde7ad1a041b1c9e761a
Ellen Marie Bartling <ellenbartling@gmail.com>
2022-06-26 09:30:57 -0700

    Update auth.js

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -18,7 +18,6 @@ export const useAuthStore = defineStore({
     loadUser() {
         this.user = supabase.auth.user();
         // ToDo: hook this up to database
-        // this.user.hasActiveGame = true
     },
     async login(data) {
       const { user, error } = await supabase.auth.signIn(data)
