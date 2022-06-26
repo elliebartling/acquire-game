792c9428535e7aefd0e1de07ee4eb5cdf35c5455
Ellen Marie Bartling <ellenbartling@gmail.com>
2022-06-26 09:27:07 -0700

    Update auth.js

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -6,6 +6,7 @@ import { supabase } from '@/supabase'
 export const useAuthStore = defineStore({
   id: 'auth',
   state: () => ({
+    loading: true,
     user: null,
     redirectRoute: null,
   }),
